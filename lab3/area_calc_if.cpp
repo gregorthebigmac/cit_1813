50b8cc0ef78bc14f14f7749a3a5ff03e6297dd78
Gregor <gregorthebigmac@gmail.com>
2019-01-29 04:29:44 -0600

    moved files where they belong and got a decent boilerplate going.

---
+++ lab3/area_calc_if.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+float triangle_area_calc(float base, float height);
+float elipse_area_calc(float axis1, float axis2);
+float trapezoid_area_calc(float base1, float base2, float height);
+
+const float pi = 3.14159265358979;
+
+int main() {
+	cout << "test" << endl;
+	return 0;
+}
+
+float triangle_area_calc(float base, float height) {
+	float _area;
+	return _area;
+}
+
+float elipse_area_calc(float axis1, float axis2) {
+	float _area;
+	return _area;
+}
+
+float trapezoid_area_calc(float base1, float base2, float height) {
+	float _area;
+	return _area;
+}
