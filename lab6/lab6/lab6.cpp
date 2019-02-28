f0c762ea90ea925699f352f272f9f6fa46944617
Gregor <gregorthebigmac@gmail.com>
2019-02-28 00:37:23 -0600

    removed unnecessary comments.

--- lab6/lab6/lab6.cpp
+++ lab6/lab6/lab6.cpp
@@ -1,32 +1,3 @@
-/*
-You have been assigned to develop a new password system for our company.Here are steps for the user in your program(50 points).
-
-1. The program first allows the user to enter a new password.The password must
-	meet the requirements below before the program allows user to go to the next step.
-2. After the new password is created, the program will ask if the user want to
-	make change to the password or to exit the program.
-3. If the user chooses to change the password, the old password will need to be
-	entered first for verification before the new password can be created.
-4. After that the user will enter the new password that must meet the requirements.
-5. After the program accepts the new password, again the program will ask if
-	the user want to make change to the password or to exit the program.
-
-Password Requirements
-
-1. Password must be at least 8 characters.
-2. Password must contain at least one number.
-3. Password must contain at least one special character.
-4. Password must contain at least one letter of the alphabet.
-5. Password must contain at least one uppercase letter.
-6. Password must contain at least one lowercase letter.
-7. Password must not contain a whitespace character such as a space, a tap (tab?), a newline character.
-8. If the user modifies the old password, the new password must not be the same as the previous password.
-
-*** Bonus 10 points *** 
-Utilizing header file(the main function will call other functions that are stored in the header file).
-
-*** Tip: Related example : The Create Account Program(pages 226 - 229) ***
-*/
 #include "pch.h"
 #include <iostream>
 #include "password.h"
